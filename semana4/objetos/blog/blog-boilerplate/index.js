2ada47c964f93b56849fd3980b7042d656afc5e5
Guilherme Henrique Mota Alfredo <guilhermehmota12@gmail.com>
2021-03-03 16:28:48 -0300

    atualizei o blog e agora é possivel que o usuário adicione uma imagem ao seu post

--- semana4/objetos/blog/blog-boilerplate/index.js
+++ semana4/objetos/blog/blog-boilerplate/index.js
@@ -7,11 +7,13 @@ let inputTitulo = document.getElementById("titulo-post")
 let inputAutor = document.getElementById("autor-post")
 let conteudoPost = document.getElementById("conteudo-post")
 let containerDePosts = document.getElementById("container-de-posts")
+let inputImagem = document.getElementById("inputImagem")
 
 const objetosInput = {
     titulo: inputTitulo.value,
     autor: inputAutor.value,
     conteudo: conteudoPost.value,
+    imagem: inputImagem.value 
 }
 
 arrayObjeto.push(objetosInput)
@@ -22,10 +24,12 @@ conteudoPost.value = ""
 
 console.log(objetosInput)
 
-containerDePosts.innerHTML += `<h1>${objetosInput.titulo}</h1>`
-containerDePosts.innerHTML += `<h2>${objetosInput.autor}</h2>`
+containerDePosts.innerHTML += `<h1>Título: ${objetosInput.titulo}</h1>`
+containerDePosts.innerHTML += `<h3>Autor: ${objetosInput.autor}</h3>`
 containerDePosts.innerHTML += `<p>${objetosInput.conteudo}</p>`
 
-} 
-
-
+if (inputImagem.value.includes(".jpg") || inputImagem.value.includes(".png") && inputImagem.value.includes("http")){
+    containerDePosts.innerHTML += `<img src=${inputImagem.value}>`
+    inputImagem.value = ""
+}
+}
