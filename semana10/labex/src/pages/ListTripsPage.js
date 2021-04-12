02650179bbd51bd5e320ddef8be7ae159f641abd
Guilherme Henrique Mota Alfredo <guilhermehmota12@gmail.com>
2021-04-12 16:27:24 -0300

    inicio do projeto, paginas criadas

---
+++ semana10/labex/src/pages/ListTripsPage.js
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+
+export default function ListTripsPage() {
+
+
+
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+
